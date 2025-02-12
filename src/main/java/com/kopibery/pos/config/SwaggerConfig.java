8cb2f383451aab7083a086d9104341e1969e11e5
benaleo <benaleobayu@gmail.com>
2025-02-12 22:24:43 +0700

    update swagger

--- src/main/java/com/kopibery/pos/config/SwaggerConfig.java
+++ src/main/java/com/kopibery/pos/config/SwaggerConfig.java
@@ -2,10 +2,15 @@ package com.kopibery.pos.config;
 
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
 import lombok.RequiredArgsConstructor;
 import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
@@ -17,6 +22,19 @@ import org.springframework.context.annotation.Configuration;
         description = "JWT Authorization header using the Bearer scheme."
 )
 public class SwaggerConfig {
+
+    @Value("${app.base.url}")
+    private String baseUrl;
+
+    @Bean
+    public OpenAPI myOpenAPI() {
+        Server appServer = new Server();
+        appServer.setUrl(baseUrl);
+        appServer.setDescription("Development Server");
+
+        return new OpenAPI()
+                .servers(List.of(appServer));
+    }
 
     // Define OpenAPI documentation group for the public API
     @Bean
