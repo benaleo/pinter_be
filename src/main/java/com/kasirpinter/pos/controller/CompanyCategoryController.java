1936f7b73952cfb9dca06243402f409050595f53
benaleo <benaleobayu@gmail.com>
2025-03-05 08:03:09 +0700

    clean controller

--- src/main/java/com/kasirpinter/pos/controller/CompanyCategoryController.java
+++ src/main/java/com/kasirpinter/pos/controller/CompanyCategoryController.java
@@ -1,23 +1,31 @@
 package com.kasirpinter.pos.controller;
 
-import com.kasirpinter.pos.model.CompanyCategoryModel;
+import java.net.URI;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.kasirpinter.pos.model.CompanyCategoryModel;
 import com.kasirpinter.pos.response.ApiResponse;
 import com.kasirpinter.pos.response.PaginationCmsResponse;
 import com.kasirpinter.pos.response.ResultPageResponseDTO;
 import com.kasirpinter.pos.service.CompanyCategoryService;
-import com.kasirpinter.pos.service.CompanyService;
+
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
-
-import java.net.URI;
 
 @RestController
 @AllArgsConstructor
