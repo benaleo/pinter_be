3cf69fda0b0b6eac262b13b7eb41acf48c1b4a1d
benaleo <benaleobayu@gmail.com>
2025-03-11 22:36:14 +0700

    test cors

--- src/main/java/com/kasirpinter/pos/KasirPinterApplication.java
+++ src/main/java/com/kasirpinter/pos/KasirPinterApplication.java
@@ -5,14 +5,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.event.EventListener;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 @SpringBootApplication(scanBasePackages = "com.kasirpinter.pos")
 @EntityScan(basePackages = "com.kasirpinter.pos.entity")
 @EnableJpaRepositories(basePackages = "com.kasirpinter.pos.repository")
 public class KasirPinterApplication {
-    
+
     @Value("${app.base.url}")
     private String baseUrl;
 
@@ -23,24 +30,25 @@ public class KasirPinterApplication {
         SpringApplication.run(KasirPinterApplication.class, args);
     }
 
-//     @Bean
-//    public CorsConfigurationSource corsConfigurationSource() {
-//        CorsConfiguration corsConfiguration = new CorsConfiguration();
-//
-//        corsConfiguration.setAllowedOrigins(Arrays.asList(
-//                "http://localhost:3000",
-//                "https://console.kasirpinter.id"
-//        ));
-//
-//        corsConfiguration.setAllowedMethods(Collections.singletonList("*"));
-//        corsConfiguration.setAllowedHeaders(Collections.singletonList("*"));
-//        corsConfiguration.setAllowCredentials(true);
-//
-//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//        source.registerCorsConfiguration("/**", corsConfiguration);
-//
-//        return source;
-//    }
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+
+        corsConfiguration.setAllowedOrigins(Arrays.asList(
+                "http://localhost:3000",
+                "https://console.kasirpinter.id",
+                "https://secret-api.kasirpinter.id"
+        ));
+
+        corsConfiguration.setAllowedMethods(Collections.singletonList("*"));
+        corsConfiguration.setAllowedHeaders(Collections.singletonList("*"));
+        corsConfiguration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+
+        return source;
+    }
 
     @EventListener(ApplicationStartedEvent.class)
     public void onApplicationStarted() {
