36350c2e67f420134e883cf17b5d80cb80fd48c1
benaleo <benaleobayu@gmail.com>
2025-03-20 08:31:05 +0700

    add cast string projection

---
+++ src/main/java/com/kasirpinter/pos/model/projection/CastStringAndStringProjection.java
@@ -0,0 +1,11 @@
+package com.kasirpinter.pos.model.projection;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class CastStringAndStringProjection {
+    private String id;
+    private String name;
+}
